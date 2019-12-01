171c28c87c85d3048e02abb7cec96c0d66a04db6
Denise Koorevaar <denise.koorevaar@icloud.com>
2019-12-01 15:28:48 +0100

    Added Project Files

---
+++ js/script.js
@@ -0,0 +1,56 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+  {
+      quote: 'Act as if what you do makes a difference. It does.', //Quote
+      source: 'William James' //Author
+  },
+  {
+      quote: 'Success is not final, failure is not fatal: it is the courage to continue that counts.', //Quote
+      source: 'Winston Churchill' //Author
+  },
+  {
+      quote: 'Never bend your head. Always hold it high. Look the world straight in the eye.', //Quote
+      source: 'Helen Keller' //Author
+  },
+  {
+      quote: 'What you get by achieving your goals is not as important as what you become by achieving your goals.', //Quote
+      source: 'Zig Ziglar', //Author
+  },
+  {
+      quote: "Believe you can and you're halfway there.", //Quote
+      source: 'Theodore Roosevelt', //Author
+      //citation: 'Test', //Where from
+      //year: 'Test' //Year of the quote
+  }
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
